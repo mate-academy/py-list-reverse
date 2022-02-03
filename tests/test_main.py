2d493ffafb70e711d9e5ba99e17c463d6e665598
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-03 23:09:45 +0200

    'Slution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import list_reverse
+
+
+def test_final_result():
+    assert list_reverse(['Mate', 'Academy']) == ['ymed', 'acAetaM']
+
+
+def test_is_element_length_correct():
+    origin_list = ['I', 'am', 'a', 'student!']
+    reversed_list = list_reverse(['I', 'am', 'a', 'student!'])
+
+    for i, j in zip(origin_list, reversed_list):
+        assert len(i) == len(j)
+
+
+def test_is_sliced_content_reversed():
+    origin_list = ['Python', 'Django', 'Flask', 'SQL', 'NoSQL']
+    reversed_list = list_reverse(origin_list)
+
+    assert "".join(origin_list) == "".join(reversed_list)[::-1]
