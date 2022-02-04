0b6b33628999663f9494392cf4810e3fa41af5aa
Denys Lipin <lipin.transstream@gmail.com>
2022-02-04 17:57:56 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.main import list_reverse
+
+
+def test_should_return_empty_list():
+    assert list_reverse([]) == []
+
+
+def test_should_return_empty_sting_in_list():
+    assert list_reverse([""]) == [""]
+
+
+def test_should_reverse_one_word_in_list():
+    assert list_reverse(["Hell012!"]) == ["!210lleH"]
+
+
+def test_should_reverse_few_words_in_list():
+    assert list_reverse(['I', 'am', 'a', 'student!']) \
+           == ['!', 'tn', 'e', 'dutsamaI']
